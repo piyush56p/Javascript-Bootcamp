1718d5d573a538e80b9aeed9839bf28f7a4f3baa
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-01-17 15:26:21 +0530

    Basic Programming Concepts

---
+++ Fundamentals-1/script.js
@@ -0,0 +1,16 @@
+//Fundamentals Part 1
+//Variables and console log
+let js = 'amazing';
+
+console.log(40+8+2);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "jonas";
+console.log(firtName);
+
+let job1 = "Programmer";
+let job2 = "teacher";
+
+let myFirstJob = "programmer";
+let my_current_job = "Teaccher";
