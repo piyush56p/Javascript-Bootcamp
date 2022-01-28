bd10be0afd820063e88fa41a96ff4994fe667e83
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-01-28 16:06:41 +0530

    Create script.js

---
+++ Project1-Guess-My-Number/script.js
@@ -0,0 +1,49 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+/*
+//DOM -> Document Object model
+Structured representation of html documents, allows javascript to access html Elements and styles to manipulate them.
+
+document.querySelector('.message').textContent = 'Correct Number ';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+//handling clicks of a button
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.getSelector('.message').textContent = 'You Lost BBygirl';
+      document.querySelector('.score').textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.getSelector('.message').textContent = 'You Lost BBygirl';
+      document.querySelector('.score').textContent = score;
+    }
+  }
+});
