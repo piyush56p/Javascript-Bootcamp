b3cd904e895c24f04999e5b74d99f74536e165f5
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-01-29 15:01:57 +0530

    Update script.js

--- Project1-Guess-My-Number/script.js
+++ Project1-Guess-My-Number/script.js
@@ -17,23 +17,29 @@ console.log(document.querySelector('.guess').value);
 */
 //handling clicks of a button
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('body').style.backgroundColor = '#222';
+document.querySelector('.message').textContent = 'Start Guessing...';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
+  //no guess
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.getSelector('.message').textContent = 'You Lost BBygirl';
+      document.querySelector('.message').textContent = 'You Lost BBygirl';
       document.querySelector('.score').textContent = score;
     }
   } else if (guess < secretNumber) {
@@ -42,8 +48,22 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.getSelector('.message').textContent = 'You Lost BBygirl';
+      document.querySelector('.message').textContent = 'You Lost BBygirl';
       document.querySelector('.score').textContent = score;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  //displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
